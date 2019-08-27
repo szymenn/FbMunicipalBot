8a58acef9906b044884b03e1aff9f9fd806ad3fb
szymenn <ilikedzimi@gmail.com>
2019-08-27 17:12:51 +0200

    Add Custom Exceptions

---
+++ FbMunicipalTransportBot/Exceptions/VerificationException.cs
@@ -0,0 +1,34 @@
+using System;
+using FbMunicipalTransportBot.Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace FbMunicipalTransportBot.Exceptions
+{
+    public class VerificationException : Exception
+    {
+        public int StatusCode { get; }
+        public string ReasonPhrase { get; }
+
+        public VerificationException()
+        {
+            StatusCode = StatusCodes.Status403Forbidden;
+            ReasonPhrase = Constants.Forbidden;
+        }
+
+        public VerificationException(string message)
+            : base(message)
+        {
+            StatusCode = StatusCodes.Status403Forbidden;
+            ReasonPhrase = Constants.Forbidden;
+
+        }
+
+        public VerificationException(string message, Exception inner)
+            : base(message, inner)
+        {
+            StatusCode = StatusCodes.Status403Forbidden;
+            ReasonPhrase = Constants.Forbidden;
+
+        }
+    }
+}
