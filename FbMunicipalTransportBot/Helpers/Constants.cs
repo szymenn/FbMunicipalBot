40eefd4952726c29a7eb862531b5937d022366bf
szymenn <ilikedzimi@gmail.com>
2019-08-27 17:14:59 +0200

    Add Constants file

---
+++ FbMunicipalTransportBot/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+namespace FbMunicipalTransportBot.Helpers
+{
+    public static class Constants
+    {
+        public const string FacebookSettings = "FacebookSettings";
+        public const string QueryMode = "hub.mode";
+        public const string QueryChallenge = "hub.challenge";
+        public const string QueryVerifyToken = "hub.verify_token";
+        public const string Forbidden = "Forbidden";
+        public const string InternalServerError = "Internal Server Error";
+        public const string BadRequest = "Bad Request";
+    }
+}
