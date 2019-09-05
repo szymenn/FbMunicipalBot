1512d1fd82a3623baee628ad6790c34c28e4b5e5
szymenn <ilikedzimi@gmail.com>
2019-09-05 18:02:21 +0200

    code cleanup

--- src/FbRestaurantsBot/Models/Messaging/Response.cs
+++ src/FbRestaurantsBot/Models/Messaging/Response.cs
@@ -5,8 +5,8 @@ namespace FbRestaurantsBot.Models.Messaging
     public class Response
     {
         [JsonProperty("recipient")]
-        public Recipient Recipient;
+        public Recipient Recipient { get; set; }
         [JsonProperty("message")]
-        public MessageResponse Message;
+        public MessageResponse Message { get; set; }
     }
 }
