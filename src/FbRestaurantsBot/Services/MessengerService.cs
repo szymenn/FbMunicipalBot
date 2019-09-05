c487ad4ff8a210fffab7366601dcfdce216651dd
szymenn <ilikedzimi@gmail.com>
2019-09-05 13:50:23 +0200

    code cleanup

--- src/FbRestaurantsBot/Services/MessengerService.cs
+++ src/FbRestaurantsBot/Services/MessengerService.cs
@@ -20,7 +20,7 @@ namespace FbRestaurantsBot.Services
         private readonly FacebookSettings _fbSettings;
         private readonly IMessengerClient _messengerClient;
         private readonly ILogger<MessengerService> _logger;
-        private IZomatoApiClient _zomatoClient;
+        private readonly IZomatoApiClient _zomatoClient;
 
         public MessengerService(IOptions<FacebookSettings> fbSettings, 
             IMessengerClient messengerClient,
@@ -38,7 +38,7 @@ namespace FbRestaurantsBot.Services
             var webHookRequest = ParseRequestBody(request);
             if (webHookRequest.Object == _fbSettings.Object)
             {
-                await CallSendApi(webHookRequest);
+                await HandleWebHookRequest(webHookRequest);
             }
             else
             {
@@ -70,39 +70,76 @@ namespace FbRestaurantsBot.Services
             }
         }
 
-        private async Task CallSendApi(WebHookRequest webHookRequest)
+        private async Task HandleWebHookRequest(WebHookRequest webHookRequest)
         {
             foreach (var entry in webHookRequest.Entry)
             {
-                var webHookEvent = entry.Messaging[0];
-                if (webHookEvent.Message != null && webHookEvent.Sender != null)
-                {
-                    if (webHookEvent.Message.Text != null)
-                    {
-                        await _messengerClient.CallSendApi(webHookEvent.Sender.Id, "hehe odpowiadam");
-                    }
-                    else if (webHookEvent.Message.Attachments != null)
-                    {
-                        var attachment = webHookEvent.Message.Attachments[0];
-                        if (attachment.Type == "location")
-                        {
-                            var nearby= await _zomatoClient.CallZomatoApi
-                            (attachment.Payload.Coordinates.Latitude,
-                                attachment.Payload.Coordinates.Longitude);
-                            _logger.LogInformation("OK");
-
-
-                            await _messengerClient.CallSendApi(webHookEvent.Sender.Id,
-                                GetRestaurantsString(nearby.Restaurants));
-                        }
-                    }
-                }
-                if (webHookEvent.Postback != null)
-                {
-                    throw new NotImplementedException();
-                }
-                
+                await HandleWebHookEvents(entry.Messaging);
             }
+        }
+
+
+        private async Task HandleWebHookEvents(IEnumerable<Messaging> webHookEvents)
+        {
+            foreach (var webHookEvent in webHookEvents)
+            {
+                await HandleWebHookEvent(webHookEvent);
+            }
+        }
+
+        private async Task HandleWebHookEvent(Messaging webHookEvent)
+        {
+            if (webHookEvent.Message != null && webHookEvent.Sender != null)
+            {
+                await HandleTextMessage(webHookEvent.Message.Text, webHookEvent.Sender.Id);
+                await HandleAttachmentsMessage(webHookEvent.Message.Attachments, webHookEvent.Sender.Id);
+            }
+            if (webHookEvent.Postback != null)
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        private async Task HandleTextMessage(string text, string senderId)
+        {
+            if (text != null)
+            {
+                await SendBasicResponse(senderId);
+            }
+        }
+
+        private async Task HandleAttachmentsMessage(IEnumerable<Attachment> attachments, string senderId)
+        {
+            if (attachments != null)
+            {
+                foreach (var attachment in attachments)
+                {
+                    if (attachment.Type == "location")
+                    {
+                        await HandleLocation(attachment, senderId);
+                    }
+                    else if (attachment.Type != null)
+                    {
+                        await SendBasicResponse(senderId);
+                    }
+                }
+            }
+        }
+
+        private async Task SendBasicResponse(string senderId)
+        {
+            await _messengerClient.CallSendApi(senderId,
+                "Please send your location in order to get nearby restaurants info");
+        }
+
+        private async Task HandleLocation(Attachment attachment, string senderId)
+        {
+            var nearby= await _zomatoClient.CallZomatoApi
+            (attachment.Payload.Coordinates.Latitude,
+                attachment.Payload.Coordinates.Longitude);
+
+            await _messengerClient.CallSendApi(senderId,
+                GetRestaurantsString(nearby.Restaurants));
         }
 
         private string GetRestaurantsString(ICollection<RestaurantWrapper> restaurants)
@@ -110,14 +147,6 @@ namespace FbRestaurantsBot.Services
             return restaurants.Aggregate("",
                 (current, next) => current + (next.Restaurant.Name + "\n" + next.Restaurant.Location.Address +
                                               "\n" + next.Restaurant.Url + "\n\n"));
-        }
-        
-
-        private async Task<Nearby> CallZomatoApi(Attachment attachment)
-        {
-            return await _zomatoClient.CallZomatoApi
-            (attachment.Payload.Coordinates.Latitude,
-                attachment.Payload.Coordinates.Longitude);
         }
         
     }
