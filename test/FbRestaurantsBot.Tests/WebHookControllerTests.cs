3ea1f394f1529ca1d03179eab4eb4afe881a8980
szymenn <ilikedzimi@gmail.com>
2019-09-06 13:52:08 +0200

    Add test project and WebHookControllerTests class

---
+++ test/FbRestaurantsBot.Tests/WebHookControllerTests.cs
@@ -0,0 +1,87 @@
+using System.Threading.Tasks;
+using FbRestaurantsBot.Controllers;
+using FbRestaurantsBot.Exceptions;
+using FbRestaurantsBot.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace FbRestaurantsBot.Tests
+{
+    public class WebHookControllerTests
+    {
+        [Fact]
+        public void Verify_ByDefault_ReturnsOkObjectResult()
+        {
+            var messengerServiceStub = new Mock<IMessengerService>();
+
+            var controller = new WebHookController(messengerServiceStub.Object);
+            var result = controller.Verify
+                (It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>());
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Verify_ByDefault_CallsMessengerService()
+        {
+            var messengerServiceMock = new Mock<IMessengerService>();
+
+            var controller = new WebHookController(messengerServiceMock.Object);
+            var result = controller.Verify
+                (It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>());
+
+            messengerServiceMock.Verify(e => e.VerifyToken
+                (It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public void Verify_WhenVerificationFailed_ThrowsVerificationException()
+        {
+            var messengerServiceStub = new Mock<IMessengerService>();
+            messengerServiceStub.Setup(e => e.VerifyToken(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<VerificationException>();
+            
+            var controller = new WebHookController(messengerServiceStub.Object);
+
+            Assert.Throws<VerificationException>(() =>
+                controller.Verify(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+        }
+
+        [Fact]
+        public async Task Receive_ByDefault_ReturnsOkResult()
+        {
+            var messengerServiceStub = new Mock<IMessengerService>();
+            
+            var controller = new WebHookController(messengerServiceStub.Object);
+            var result = await controller.Receive();
+
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task Receive_ByDefault_CallsMessengerService()
+        {
+            var messengerServiceMock = new Mock<IMessengerService>();
+            
+            var controller = new WebHookController(messengerServiceMock.Object);
+            var result = await controller.Receive();
+
+            messengerServiceMock.Verify(e => e.Receive(It.IsAny<HttpRequest>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Receive_WhenIncorrectRequestBody_ThrowsMessengerException()
+        {
+            var messengerServiceMock = new Mock<IMessengerService>();
+            messengerServiceMock.Setup(e => e.Receive(It.IsAny<HttpRequest>()))
+                .Throws<MessengerException>();
+            
+            var controller = new WebHookController(messengerServiceMock.Object);
+
+            await Assert.ThrowsAsync<MessengerException>
+                (() => controller.Receive());
+        }
+    }
+}
